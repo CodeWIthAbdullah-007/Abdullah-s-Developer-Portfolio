223e83a197c786133c31a71fde511f520b769fd6
CodeWithAbdullah-007 <mabdullahanwar85@gmail.com>
2026-01-16 04:22:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-new Typed('#element',{
-    strings:['Web Developer','Web Designer','UI / UX Designer','Video Editor'],
-    typeSpeed:60,
-    loop:true
+new Typed('#element', {
+    strings: ['Web Developer', 'Web Designer', 'UI / UX Designer', 'Video Editor'],
+    typeSpeed: 60,
+    loop: true
 });
 
-document.getElementById('darkBtn').onclick=()=>document.body.classList.remove("light");
-document.getElementById('lightBtn').onclick=()=>document.body.classList.add("light");
+const reveals = document.querySelectorAll(".reveal");
+window.addEventListener("scroll", () => {
+    reveals.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) { el.classList.add("active") }
+    });
+});
+
+document.getElementById('darkBtn').onclick = () => document.body.classList.remove("light");
+document.getElementById('lightBtn').onclick = () => document.body.classList.add("light");
